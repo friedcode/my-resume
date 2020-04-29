e69107a300f7fa3adca8ca2a7ea74351d952e874
friedcode <fried.m1994@gmail.com>
2020-04-29 15:21:43 +0200

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,8 @@
+let resumeData = {
+  "imagebaseurl": "http://localhost:3000/",
+  "name": "Michael Fried",
+  "role": "Software Engineer",
+  "location": "Constance"
+}
+
+export default resumeData 
