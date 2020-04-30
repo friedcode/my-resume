52bc45df4c3e5a32f562e0403ce31aa1f30cade1
friedcode <fried.m1994@gmail.com>
2020-04-30 12:36:19 +0200

    added release notes

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -91,10 +91,11 @@ export default class Resume extends Component {
               <h1><span>Skills</span></h1>
             </div>
             <div className="nine columns main-col">
-              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-              eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-              voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequi nesciunt.
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+                voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt.
             </p>
               <div className="bars">
                 <ul className="skills">
